eef9935045f3ba443af7d6bfbf118351e2154909
Eric Furukawa <eric.furukawa@gmail.com>
2021-11-23 09:49:31 -0800

    added inquirer and initial prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,57 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a description of the project'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the installation instructions?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the usage instructions?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How does someone contribute to the project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are the test intructions?'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What languages does the project use?',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
+        
+    ]);
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -11,3 +60,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+questions();
